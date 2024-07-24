378a21f29ba414259236be1160a5ebedbb182a08
Audyari Wiyono <audy123ari@gmail.com>
2024-07-24 16:37:39 +0700

    4. Basic Utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      }
+    },
+    fontFamily: {
+      'playwrite': ['Playwrite BE VLG', 'sans-serif'],
+    }
   },
   plugins: [],
 }
-
